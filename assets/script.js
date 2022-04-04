7cb84344f57d7e0f605fb5aa0860bc299f1756b4
joey bennett <joeykb17@gmail.com>
2022-04-03 17:24:04 -0700

    add pseudocode comments to brainstorm

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// start game with button
+// to do this we will need an addeventlistener with
+// a quiz function
+
+// the quiz function will likely be the mother function
+// she will have all the other functions within
+
+// there will need to be a function that displays
+// the current question, with all the children
+// answers/responses
+
+// there will need to be a function that holds each question?
+// possibly a questions object that holds all the 
+// questions as objects and the questions have
+// answers as values within
+
+// there will need to be a timer function
+// we need to ensure that time is subtracted from
+// this timer when you answer incorrectly
+
+// gameOver function, which should be the same for
+// either completing all the questions or running out 
+// of time
+
+// highscores page which holds all the scores on localStorage
